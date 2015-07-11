e64c5b122022f1ebd25630cf316a2dc810e30e87
Mikhail Khazov <mikhailkhazov@gmail.com>
2015-07-11 16:35:39 +0300

    Код переписан на ООП

--- app/generate-img.php
+++ app/generate-img.php
@@ -1,20 +1,21 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     require_once '../vendor/autoload.php';
 
     // Получаем файлы основной картинки и водяного знака
-    $image = check_file($_FILES['image']);
-    $watermark = check_file($_FILES['watermark']);
+    $image = $_FILES['image'];
+    $watermark = $_FILES['watermark'];
 
-    $position_x = $_POST['position_x'];
-    $position_y = $_POST['position_y'];
+    // Получаем координаты
+    $position_x = $_POST['position-x'];
+    $position_y = $_POST['position-y'];
+    // Получаем значение прозрачности
     $opacity = $_POST['opacity'];
-    // @todo validate POST data
 
-    // @todo pass coordinates and opacity values as well
-    process_image($image, $watermark);
+    $wg = new WatermarkGenerator($image, $watermark, $position_x, $position_y, $opacity);
+    $wg->process_image();
 }
 
 else {
@@ -23,131 +24,225 @@ else {
     exit;
 }
 
-/**
- * Функция-обертка для всех действий с картинками.
- * @param array $image POST-массив файла основного изображения.
- * @param array $watermark POST-массив файла изображения водяного знака.
- * @param int $position_x x-координата положения водяного знака.
- * @param int $position_y y-координата положения водянего знака.
- * @param int $opacity прозрачность водяного знака.
- */
-function process_image($image, $watermark, $position_x = 0, $position_y = 0, $opacity = 100) {
-    // генерация изображения
-    $result_image = generate_img($image['tmp_name'], $watermark['tmp_name'], $position_x, $position_y, $opacity);
-    // генерация имени файла, предлагаемого для сохранения
-    $result_filename = generate_filename($image['name']);
-    // диалог сохранения файла
-    download_img($result_image, $result_filename, mb_strlen($result_image));
-}
+class WatermarkGenerator {
+    const IMG_FORMAT = 'jpg';
+    const JPEG_QUALITY = 80;
 
-/**
- * Генерация изображения.
- *
- * @param string $image_file файл основного изображения.
- * @param string $watermark_file файл водяного знака.
- * @param int $position_x требуемая позиция водяного знака по оси X.
- * @param int $position_y требуемая позиция водяного знака по оси Y.
- * @return string сгенерированное изображение (binary string).
- */
-function generate_img($image_file, $watermark_file, $position_x = 0, $position_y = 0, $opacity = 100) {
-    $jpeg_quality = 80;
+    private $image;
+    private $watermark;
+    private $image_file;
+    private $watermark_file;
+    private $position = array('x' => 0, 'y' => 0);
+    private $opacity = 100;
 
-    $wi = new WideImage\WideImage();
-    $image = $wi->load($image_file);
-    $watermark = $wi->load($watermark_file);
+    /**
+     * @param array $image POST-массив файла основного изображения.
+     * @param array $watermark POST-массив файла изображения водяного знака.
+     * @param int $position_x x-координата положения водяного знака.
+     * @param int $position_y y-координата положения водяного знака.
+     * @param int $opacity прозрачность водяного знака.
+     */
+    public function __construct($image, $watermark, $position_x, $position_y, $opacity) {
+        $this->image_file = $image;
+        $this->watermark_file = $watermark;
+        $this->position = array(
+            'x' => isset($position_x) ? $position_x : $this->position['x'],
+            'y' => isset($position_y) ? $position_y : $this->position['y'],
+        );
+        $this->opacity = isset($opacity) ? $opacity : $this->opacity;
 
-    $generated_image = $image->merge($watermark, $position_x, $position_y, $opacity);
-    $result_image = $generated_image->asString('jpg', $jpeg_quality);
-
-    return $result_image;
-}
-
-/**
- * @param string $img итоговая картинка (binary string).
- * @param string $filename имя файла для сохранения на клиенте.
- * @param int $filesize размер файла.
- */
-function download_img($img, $filename, $filesize = 0) {
-    // заставляем браузер показать окно сохранения файла
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename=' . $filename);
-    header('Content-Transfer-Encoding: binary');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . $filesize);
-    // отправляем картинку пользователю
-    echo $img;
-    exit;
-}
-
-/**
- * Генерация имени итогового файла.
- * @param array $filename имя файла загруженного изображения.
- * @return string имя итогового файла.
- */
-function generate_filename($filename) {
-    $path_parts = pathinfo($filename);
-    $result_filename = $path_parts['filename'] . '_with-wm.' . $path_parts['extension'];
-    return $result_filename;
-}
-
-/**
- * Валидация загружаемого файла.
- * @param array $file массив-файл, который нужно проверить.
- * @return array $file
- */
-function check_file($file) {
-    if (check_file_size($file['size']) && check_file_name($file['name']) && check_file_extension($file['name'])) {
-        return $file;
+        $wi = new WideImage\WideImage();
+        $this->image = $wi->load($this->image_file['tmp_name']);
+        $this->watermark = $wi->load($this->watermark_file['tmp_name']);
     }
-    else {
-        header("HTTP/1.1 302 Moved Temporarily");
-        header("Location: /");
+
+    /**
+     * Все действия с картинками.
+     */
+    public function process_image() {
+        if ($this->check_data()) {
+            // генерация изображения
+            $result_image = $this->generate_img($this->image, $this->watermark);
+            // генерация имени файла, предлагаемого для сохранения
+            $result_filename = $this->generate_filename($this->image_file['name']);
+            // диалог сохранения файла
+            $this->download_img($result_image, $result_filename, mb_strlen($result_image));
+        }
+        else {
+            header("HTTP/1.1 302 Moved Temporarily");
+            header("Location: /");
+            exit;
+        }
+    }
+
+    /**
+     * Валидация данных.
+     *
+     * @return bool
+     */
+    private function check_data() {
+        return
+            $this->check_file($this->image_file) &&
+            $this->check_file($this->watermark_file) &&
+            $this->check_position($this->position, $this->image_file['tmp_name']) &&
+            $this->check_opacity($this->opacity);
+    }
+
+    /**
+     * Генерация изображения.
+     *
+     * @return string сгенерированное изображение (binary string).
+     */
+    private function generate_img() {
+        $result_image = $this->image
+            // наложение водяного знака
+            ->merge($this->watermark, $this->position['x'], $this->position['y'], $this->opacity)
+            // полученное изображение в виде строки
+            ->asString(self::IMG_FORMAT, self::JPEG_QUALITY);
+
+        return $result_image;
+    }
+
+    /**
+     * Вызов диалога сохранения файла в браузере.
+     *
+     * @param string $img итоговая картинка (binary string).
+     * @param string $filename имя файла для сохранения на клиенте.
+     * @param int $filesize размер файла.
+     */
+    private function download_img($img, $filename, $filesize = 0) {
+        // заставляем браузер показать окно сохранения файла
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . $filename);
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . $filesize);
+        // отправляем картинку пользователю
+        echo $img;
         exit;
     }
-}
 
-/**
- * Валидация расширения файла.
- * @param string $filename имя файла.
- * @return bool валидно ли расширение файла.
- */
-function check_file_extension($filename) {
-    $allowed_extensions = array('gif', 'png', 'jpg', 'bmp');
-    $path_parts = pathinfo($filename);
-    $extension = strtolower($path_parts['extension']);
-    if (in_array($extension, $allowed_extensions)) {
-        return TRUE;
+    /**
+     * Генерация имени итогового файла.
+     *
+     * @param array $filename имя файла загруженного изображения.
+     * @return string имя итогового файла.
+     */
+    private function generate_filename($filename) {
+        $path_parts = pathinfo($filename);
+        $result_filename = $path_parts['filename'] . '_with-wm.' . $path_parts['extension'];
+        return $result_filename;
     }
-    else {
+
+    /**
+     * Валидация загружаемого файла.
+     *
+     * @param array $file массив-файл, который нужно проверить.
+     * @return bool
+     */
+    private function check_file($file) {
+        if (is_array($file) && !empty($file['size']) && !empty($file['name'])) {
+            if ($this->check_file_size($file['size']) &&
+                $this->check_file_name($file['name']) &&
+                $this->check_file_extension($file['name'])) {
+                return TRUE;
+            }
+        }
+
         return FALSE;
     }
-}
 
-/**
-* Проверка размера файла.
-*
-* @param string $filesize размер файла.
-* @return bool валиден ли размер файла.
-*/
-function check_file_size($filesize) {
-    // файл не пустой и не больше 2 мб
-    return ($filesize = 0 || $filesize > 2097152) ? false : true;
-}
+    /**
+     * Валидация расширения файла.
+     *
+     * @param string $filename имя файла.
+     * @return bool валидно ли расширение файла.
+     */
+    private function check_file_extension($filename) {
+        $allowed_extensions = array('gif', 'png', 'jpg', 'bmp');
+        $path_parts = pathinfo($filename);
+        $extension = strtolower($path_parts['extension']);
+        if (in_array($extension, $allowed_extensions)) {
+            return TRUE;
+        }
+        else {
+            return FALSE;
+        }
+    }
 
-/**
-* Проверка имени файла.
-*
-* @param string $filename имя файла.
- * @return bool валидно ли имя файла.
-*/
-function check_file_name($filename) {
-    return
-        // имя может содержать только англ. и рус. буквы, цифры, символы "_(.)- "
-        (!preg_match("{^[-0-9a-zA-Zа-яА-Я_\. \(\)]+$}i", $filename)
-        // длина не более 255 символов
-        || mb_strlen($filename,"UTF-8") > 225)
-        ? false : true;
+    /**
+     * Проверка размера файла.
+     *
+     * @param string $filesize размер файла.
+     * @return bool валиден ли размер файла.
+     */
+    private function check_file_size($filesize) {
+        // файл не пустой и не больше 2 мб
+        return ($filesize = 0 || $filesize > 2097152) ? FALSE : TRUE;
+    }
+
+    /**
+     * Проверка имени файла.
+     *
+     * @param string $filename имя файла.
+     * @return bool валидно ли имя файла.
+     */
+    private function check_file_name($filename) {
+        return
+            // имя может содержать только англ. и рус. буквы, цифры, символы "_(.)- "
+            (!preg_match("{^[-0-9a-zA-Zа-яА-Я_\. \(\)]+$}i", $filename)
+                // длина не более 255 символов
+                || mb_strlen($filename,"UTF-8") > 225)
+                ? FALSE : TRUE;
+    }
+
+    /**
+     * Валидация координат положения водяного знака относительно основного изображения.
+     *
+     * @param array $position array('x' => $position_x, 'y' => $position_y)
+     * @param string $image_filename
+     * @return bool
+     */
+    private function check_position($position, $image_filename) {
+        $errors = array();
+
+        $wi = new WideImage\WideImage();
+        $image = $wi->load($image_filename);
+
+        $width = $image->getWidth();
+        $height = $image->getHeight();
+
+        if ($position['x'] > $width) {
+            $errors[] = 'Позиция по оси X выходит за границу изображения.';
+        }
+        if ($position['y'] > $height) {
+            $errors[] = 'Позиция по оси Y выходит за границу изображения.';
+        }
+        if ($position['x'] < 0) {
+            $errors[] = 'Позиция по оси X не может иметь отрицательное значение.';
+        }
+        if ($position['y'] < 0) {
+            $errors[] = 'Позиция по оси Y не может иметь отрицательное значение.';
+        }
+
+        if (empty($errors)) {
+            return TRUE;
+        }
+        else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Валидация значения прозрачности.
+     *
+     * @param int $opacity
+     * @return bool
+     */
+    private function check_opacity($opacity) {
+        return (is_int($opacity) && $opacity >= 0 && $opacity <= 100) ? TRUE : FALSE;
+    }
 }
